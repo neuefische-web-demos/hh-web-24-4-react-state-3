5cedffbcdce78746d683d3e10c1b102262e9322c
Aleksandar Jovanović <alexxandar@gmail.com>
2024-04-26 15:16:11 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# React State 3: Demo Start
+
+This is the start state of the demo app.
+
+## Notes
+
+TO DO:
+
+- You only have to touch the `./src/App.js` file.
+
+## Development
+
+### Local Development
+
+To work locally, please install the dependencies using `npm i` first.
+
+Run `npm run start` to start a development server and open the displayed URL in a browser.
+
+### CodeSandbox Cloud
+
+Select the "Preview: 3000" tab to view this project.
+
+> The `npm run start` script runs automatically.
+
+### Scripts
+
+You can use the following commands:
+
+- `npm run start` to start a development server
---
+++ src/components/Movie/index.js
@@ -0,0 +1,32 @@
+import "./Movie.css";
+
+export default function Movie({ name, isLiked, onDeleteMovie, id, onToggleLike }) {
+  return (
+    <section className="movie">
+      <h2 className={`movie__title${isLiked ? " movie__title--is-liked" : ""}`}>
+        {name}
+      </h2>
+      <div className="movie__actions">
+        <button
+          onClick={() => onToggleLike(id)}
+          className="movie__button"
+          type="button"
+          title={isLiked ? "unlike movie" : "like movie"}
+        >
+          {isLiked ? (
+            <span role="img" aria-label="Thumbs up">
+              👍
+            </span>
+          ) : (
+            <span role="img" aria-label="Thumbs down">
+              👎
+            </span>
+          )}
+        </button>
+        <button className="movie__button" type="button" title="delete movie" onClick={() => onDeleteMovie(id)}>
+          ✕
+        </button>
+      </div>
+    </section>
+  );
+}
